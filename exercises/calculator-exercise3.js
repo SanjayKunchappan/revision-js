eca46d1ec7f6659acb0b7c3d986d9f53299c3ef9
sanjay <sanjayy7890@gmail.com>
2022-11-18 14:53:46 -0500

    addition using js

---
+++ exercises/calculator-exercise3.js
@@ -0,0 +1,11 @@
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+var firstNumber = prompt("Enter first number!");
+// 3. Prompts the user for the second number.
+var secondNumber = prompt("Enter second number!");
+// 4. stores that number and responds with the SUM by using an alert.  
+var sum = Number(firstNumber) + Number(secondNumber);
+alert("The sum of the two numbers is" + sum);
+// BONUS: Make a program that can subtract, multiply, and also divide!
