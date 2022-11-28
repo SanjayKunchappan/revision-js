24786c9b00c60c70233cf29479cbb62f09b280f6
sanjay <sanjayy7890@gmail.com>
2022-11-28 18:03:38 -0500

    objects intro

---
+++ DataStructures/Objects/objects.js
@@ -0,0 +1,19 @@
+//object example
+// objects have properties and values
+
+var user = {
+    name: "Khushi",//name is property and khushi is value
+    age: 27,
+    hobby: "painting",
+    isMarried:false,
+};
+
+// user -> would return 
+// {name: 'Khushi', age: 27, hobby: 'painting', isMarried: false}
+// age: 27
+// hobby: "painting"
+// isMarried: false
+// name: "Khushi"
+
+//user.name would return 'Khushi'
+//user.isMarried would return false
