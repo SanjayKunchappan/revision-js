6a62de79100cca5a30599bf8f6622da977703da7
sanjay <sanjayy7890@gmail.com>
2022-11-29 21:57:14 -0500

    exercise with objects

---
+++ exercises/exercise7-Objects.js
@@ -0,0 +1,27 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+{
+    username:"Sanjay", 
+    password:"khushi",
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {username:"Sanjay", password:"khushi"}
+];
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Aman",
+		timeline: "Bangalore sucks!"
+	},
+	{
+		username: "Khushi",
+		timeline: "Duniya ********* hai!"
+	},
+	{
+		username: "Sanjeev",
+		timeline: "I'm getting married!!!"
+	}
+];
