debeb3708dee9ac2a8f6148bad94580dd2b986c6
sanjay <sanjayy7890@gmail.com>
2022-11-29 22:09:08 -0500

    facebook app using js

---
+++ DataStructures/Objects/facebookapp/script.js
@@ -0,0 +1,35 @@
+var database = [
+    {
+        username: "Sanjay",
+        password: "sanjaysanjay"
+    }
+];
+
+var newsfeed = [
+	{
+		username: "Aman",
+		timeline: "Bangalore sucks!"
+	},
+	{
+		username: "Khushi",
+		timeline: "Duniya ********* hai!"
+	},
+	{
+		username: "Sanjeev",
+		timeline: "I'm getting married!!!"
+	}
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username && pass === database[0].password) {
+        console.log(newsfeed);
+    }
+    else {
+        alert("Sorry, wrong username and password!");
+    }
+}
+
+signIn (userNamePrompt, passwordPrompt);
